197c6957f2aee6c9629343e701a0f3c04af50418
kkar <kkar@dyn-2-88.lan.futurice.org>
2011-06-06 16:20:23 +0300

    Initial Commit

---
+++ Proto3Tests/Proto3Tests.h
@@ -0,0 +1,17 @@
+//
+//  Proto3Tests.h
+//  Proto3Tests
+//
+//  Created by kkar on 6/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Proto3Tests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ Proto3Tests/Proto3Tests.m
@@ -0,0 +1,33 @@
+//
+//  Proto3Tests.m
+//  Proto3Tests
+//
+//  Created by kkar on 6/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Proto3Tests.h"
+
+
+@implementation Proto3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Proto3Tests");
+}
+
+@end
